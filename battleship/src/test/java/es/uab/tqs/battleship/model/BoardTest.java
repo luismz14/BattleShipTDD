9d5916ea5d821447ae3a765bc973db46e6026d56
LevonKG <levonkesoyan@gmail.com>
2025-10-13 15:42:40 +0200

    tests for getCellState

--- battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
+++ battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
@@ -1,6 +1,9 @@
 package es.uab.tqs.battleship.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
@@ -14,4 +17,23 @@ public class BoardTest {
         Board board = new Board(10);
         // assertEquals(CellState.WATER, board.getCellState(0, 0));
     }
+
+    @Test
+    void testGetCellStateReturnsWaterInitially() {
+        Board board = new Board(10);
+        assertEquals(CellState.WATER, board.getCellState(0, 0));
+        assertEquals(CellState.WATER, board.getCellState(5, 5));
+        assertEquals(CellState.WATER, board.getCellState(9, 9));
+    }
+
+    @Test
+    void testGetCellStateThrowsExceptionForInvalidCoordinates() {
+        Board board = new Board(10);
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getCellState(-1, 0);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getCellState(10, 0);
+        });
+}
 }

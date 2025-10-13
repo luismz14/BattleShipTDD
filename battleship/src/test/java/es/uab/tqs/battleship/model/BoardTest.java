be18026209bebf1fe30f5f3c3612efb45d7b7d6f
LevonKG <levonkesoyan@gmail.com>
2025-10-13 15:40:06 +0200

    basic tests

--- battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
+++ battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
@@ -1,4 +1,17 @@
 package es.uab.tqs.battleship.model;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
 public class BoardTest {
+    @Test
+    void testCreateBoard() {
+        Board board = new Board(10);
+        assertNotNull(board);
+    }
+    @Test
+    void testBoardInitializedWithWater() {
+        Board board = new Board(10);
+        // assertEquals(CellState.WATER, board.getCellState(0, 0));
+    }
 }

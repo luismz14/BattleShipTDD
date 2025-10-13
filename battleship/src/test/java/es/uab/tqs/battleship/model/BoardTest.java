bffdc03f693a7cee0bc6ad71c38e1ec9325c7e7f
LevonKG <levonkesoyan@gmail.com>
2025-10-13 16:49:42 +0200

    refactor

--- battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
+++ battleship/src/test/java/es/uab/tqs/battleship/model/BoardTest.java
@@ -11,60 +11,55 @@ public class BoardTest {
         Board board = new Board(10);
         assertNotNull(board);
     }
+
     @Test
-    void testBoardInitializedWithWater() {
-        Board board = new Board(10);
-        assertEquals(CellState.WATER, board.getCellState(0, 0));
+    void testCreateBoardWithInvalidSize() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Board(4);
+        });
     }
 
     @Test
-    void testGetCellStateReturnsWaterInitially() {
+    void testBoardInitialized() {
         Board board = new Board(10);
-        assertEquals(CellState.WATER, board.getCellState(0, 0));
-        assertEquals(CellState.WATER, board.getCellState(5, 5));
-        assertEquals(CellState.WATER, board.getCellState(9, 9));
+        assertEquals(CellState.WATER, board.getCellState(0, 0, true));
+        assertEquals(CellState.UNKNOWN, board.getCellState(0, 0, false));
+    }
+
+    @Test
+    void testGetCellStateReturnsUnknownInitially() {
+        Board board = new Board(10);
+        assertEquals(CellState.WATER, board.getCellState(0, 0, true));
+        assertEquals(CellState.UNKNOWN, board.getCellState(0, 0, false));
+        assertEquals(CellState.WATER, board.getCellState(5, 5, true));
+        assertEquals(CellState.UNKNOWN, board.getCellState(5, 5, false));
+        assertEquals(CellState.WATER, board.getCellState(9, 9, true));
+        assertEquals(CellState.UNKNOWN, board.getCellState(9, 9, false));
     }
 
     @Test
     void testGetCellStateThrowsExceptionForInvalidCoordinates() {
         Board board = new Board(10);
         assertThrows(IndexOutOfBoundsException.class, () -> {
-            board.getCellState(-1, 0);
+            board.getCellState(-1, 0, true);
         });
         assertThrows(IndexOutOfBoundsException.class, () -> {
-            board.getCellState(10, 0);
+            board.getCellState(-1, 0, false);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getCellState(10, 0, true);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getCellState(10, 0, false);
         });
     }
 
     @Test
-    void testPlaceShipOnBoard() {
+    void testSetCellState() {
         Board board = new Board(10);
-        board.setCellState(0, 0, CellState.SHIP);
-        assertEquals(CellState.SHIP, board.getCellState(0, 0));
-    }
-
-    @Test
-    void testShootWater() {
-        Board board = new Board(10);
-        CellState result = board.shoot(0, 0);
-        assertEquals(CellState.WATER, result);
-    }
-
-    @Test
-    void testShootShip() {
-        Board board = new Board(10);
-        board.setCellState(0, 0, CellState.SHIP);
-        CellState result = board.shoot(0, 0);
-        assertEquals(CellState.HIT, result);
-        assertEquals(CellState.HIT, board.getCellState(0, 0));
-    }
-
-    @Test
-    void testCannotShootSamePositionTwice() {
-        Board board = new Board(10);
-        board.shoot(0, 0);
-        assertThrows(IllegalStateException.class, () -> {
-            board.shoot(0, 0);
-        });
+        board.setCellState(0, 0, CellState.SHIP, true);
+        assertEquals(CellState.SHIP, board.getCellState(0, 0, true));
+        board.setCellState(0, 0, CellState.HIT, false);
+        assertEquals(CellState.HIT, board.getCellState(0, 0, false));
     }
 }

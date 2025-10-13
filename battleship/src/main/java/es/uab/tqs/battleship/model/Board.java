48bc1bb9d139fe05250309c67d8538eee247d754
LevonKG <levonkesoyan@gmail.com>
2025-10-13 15:27:05 +0200

    initial model

--- battleship/src/main/java/es/uab/tqs/battleship/model/Board.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/Board.java
@@ -5,16 +5,12 @@ public class Board {
     private final CellState[][] grid;
 
     //CAMBIAR ESTO, EN EL VIEW LEER EL PLAYERID, ATRIBUTO PLAYERID Y CONSTRUIR TABLERO CON WATER
-    public Board(int size, int playerId) {
+    public Board(int size) {
         this.size = size;
         this.grid = new CellState[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (playerId == 1) {
-                    grid[i][j] = CellState.WATER; // Player's board starts with WATER
-                } else {
-                    grid[i][j] = CellState.UNKNOWN; // CPU board starts with UNKNOWN
-                }
+                    grid[i][j] = CellState.WATER;
             }
         }
     }

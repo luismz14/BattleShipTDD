fd9e951280feeab56b1db6e21fb9d07608ebbd68
LevonKG <levonkesoyan@gmail.com>
2025-11-10 16:06:55 +0100

    coordinate class

---
+++ battleship/src/main/java/es/uab/tqs/battleship/model/Coordinate.java
@@ -0,0 +1,44 @@
+package es.uab.tqs.battleship.model;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final int x;
+    private final int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isValid(int boardSize) {
+        return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinate)) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
+

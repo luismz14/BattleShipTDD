1458213ece32cf89cad3d58f6063d4e5d87d3864
LevonKG <levonkesoyan@gmail.com>
2025-11-12 17:09:11 +0100

    gameView interface added

---
+++ battleship/src/main/java/es/uab/tqs/battleship/view/GameView.java
@@ -0,0 +1,25 @@
+package es.uab.tqs.battleship.view;
+
+import es.uab.tqs.battleship.model.AttackResult;
+import es.uab.tqs.battleship.model.Board;
+import es.uab.tqs.battleship.model.Coordinate;
+
+public interface GameView {
+
+    void displayBoard(Board board, boolean hideShips);
+
+    void displayMessage(String message);
+
+    void displayAttackResult(AttackResult result);
+
+    Coordinate getCoordinateInput(String prompt);
+
+    void displayGameResult(String winner);
+
+    void displayInstructions();
+
+    void clearScreen();
+
+    boolean getConfirmation(String prompt);
+}
+

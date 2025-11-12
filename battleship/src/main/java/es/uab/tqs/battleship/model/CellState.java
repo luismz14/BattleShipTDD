8cb17d48f0c8420e6fcc9733196ae5cfe083138b
LevonKG <levonkesoyan@gmail.com>
2025-11-12 16:30:10 +0100

    cellState enum changed

--- battleship/src/main/java/es/uab/tqs/battleship/model/CellState.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/CellState.java
@@ -1,9 +1,9 @@
 package es.uab.tqs.battleship.model;
 
 public enum CellState {
-    UNKNOWN, // rival's cell not yet targeted
-    WATER,   // targeted rival's cell with no ship or player's empty cell 
-    HIT,     // ship cell that has been hit
-    SUNK,    // ship cell that has been sunk
+    EMPTY, // rival's cell not yet targeted
     SHIP,    // player's cell that contains a ship
+    HIT,     // ship cell that has been hit
+    MISS,    // empty cell that has been targeted (water)
 }
+

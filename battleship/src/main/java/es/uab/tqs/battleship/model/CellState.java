ca5fdf9ba4f90b91feeb6524da1e2c2916c87fb9
LevonKG <levonkesoyan@gmail.com>
2025-12-06 13:27:59 +0100

    cellstate commented

--- battleship/src/main/java/es/uab/tqs/battleship/model/CellState.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/CellState.java
@@ -1,9 +1,13 @@
 package es.uab.tqs.battleship.model;
 
+/**
+ * Represents the possible states of a cell on the game board.
+ * These states help track the progress of the game, including ship positions
+ * and attack results.
+ */
 public enum CellState {
     EMPTY, // rival's cell not yet targeted
-    SHIP,    // player's cell that contains a ship
-    HIT,     // ship cell that has been hit
-    MISS,    // empty cell that has been targeted (water)
+    SHIP, // player's cell that contains a ship
+    HIT, // ship cell that has been hit
+    MISS, // empty cell that has been targeted (water)
 }
-

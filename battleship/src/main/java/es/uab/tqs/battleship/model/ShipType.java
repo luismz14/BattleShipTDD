61a77e48b572c26e4e60e2001ead753e647ee891
LevonKG <levonkesoyan@gmail.com>
2025-12-06 13:37:32 +0100

    shiptype commented

--- battleship/src/main/java/es/uab/tqs/battleship/model/ShipType.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/ShipType.java
@@ -1,5 +1,9 @@
 package es.uab.tqs.battleship.model;
 
+/**
+ * Enumeration representing the different types of ships available in the game.
+ * Each ship type has a predefined length and a display name.
+ */
 public enum ShipType {
     CARRIER(5, "Carrier"),
     BATTLESHIP(4, "Battleship"),
@@ -10,15 +14,31 @@ public enum ShipType {
     private final int length;
     private final String displayName;
 
+    /**
+     * Constructor for the ShipType enum.
+     *
+     * @param length      The length of the ship.
+     * @param displayName The display name of the ship.
+     */
     ShipType(int length, String displayName) {
         this.length = length;
         this.displayName = displayName;
     }
 
+    /**
+     * Gets the length of the ship.
+     *
+     * @return The length in cells.
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * Gets the display name of the ship.
+     *
+     * @return The name of the ship.
+     */
     public String getDisplayName() {
         return displayName;
     }

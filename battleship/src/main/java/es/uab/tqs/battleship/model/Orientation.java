9f8d6ee079a111914ad04550562e0893d38bd033
LevonKG <levonkesoyan@gmail.com>
2025-12-06 13:33:44 +0100

    orientation commented

--- battleship/src/main/java/es/uab/tqs/battleship/model/Orientation.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/Orientation.java
@@ -1,5 +1,9 @@
 package es.uab.tqs.battleship.model;
 
+/**
+ * Represents the possible orientations for placing a ship on the board.
+ * Ships can be placed either horizontally or vertically.
+ */
 public enum Orientation {
     HORIZONTAL,
     VERTICAL

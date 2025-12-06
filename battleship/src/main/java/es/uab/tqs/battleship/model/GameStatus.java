45dcf28537a32fe620d9c0e35a33eb5e193a659a
LevonKG <levonkesoyan@gmail.com>
2025-12-06 13:33:36 +0100

    gameStatus commented

--- battleship/src/main/java/es/uab/tqs/battleship/model/GameStatus.java
+++ battleship/src/main/java/es/uab/tqs/battleship/model/GameStatus.java
@@ -1,10 +1,15 @@
 package es.uab.tqs.battleship.model;
 
+/**
+ * Represents the various states the game can be in during its lifecycle.
+ * These states control the flow from setup to gameplay turns and finally to the
+ * end result.
+ */
 public enum GameStatus {
-    SETUP,           
-    PLAYER_TURN,     
-    COMPUTER_TURN,   
-    PLAYER_WON,      
-    COMPUTER_WON,    
-    DRAW             
+    SETUP,
+    PLAYER_TURN,
+    COMPUTER_TURN,
+    PLAYER_WON,
+    COMPUTER_WON,
+    DRAW
 }
